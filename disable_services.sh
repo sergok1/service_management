9510beca921b737d1e5562996233d1ab013f7608
sergok1 <sergok11@gmail.com>
2025-11-23 18:40:35 +0300

    создание скриптов

---
+++ disable_services.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Остановить и отключить автозапуск сервисов мониторинга
+
+SERVICES=(
+  "dcservice.service"
+  "kesl.service"
+  "klnagent64.service"
+  "kaspersky-agent-check"
+  "kaspersky-agent-check.service"
+  "kaspersky-agent-check.timer"
+)
+
+LOG="/var/log/services_management.log"
+echo "=== [$(date)] Отключение сервисов мониторинга ===" | tee -a "$LOG"
+
+for svc in "${SERVICES[@]}"; do
+  echo "--- [$svc] ---" | tee -a "$LOG"
+  sudo systemctl stop "$svc" 2>>"$LOG"
+  sudo systemctl disable "$svc" 2>>"$LOG"
+  sudo systemctl status "$svc" --no-pager | tee -a "$LOG"
+done
+
+echo "=== [$(date)] Отключение завершено ===" | tee -a "$LOG"
